66df096f2c0831e6a2a968fbce97d1b05978dc50
Kity Dev <kitydeveloper@gmail.com>
2024-10-05 18:25:25 -0500

    Cambios de ejemplo para wsl

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Comentario para probar cambios de git
+Console.WriteLine("Este archivo fue modificaod desde WSL");
